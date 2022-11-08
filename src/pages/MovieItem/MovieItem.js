f93d5a19e71f0b4dc2d5709dae19906b4c7374e4
vlmakh <vlmakh@gmail.com>
2022-11-08 13:41:35 +0200

    add return from Cast to Movies

--- src/pages/MovieItem/MovieItem.js
+++ src/pages/MovieItem/MovieItem.js
@@ -1,6 +1,6 @@
 import css from './MovieItem.module.css';
 import { Box } from 'components/Box/Box';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import {
   Outlet,
   NavLink,
@@ -14,57 +14,64 @@ export const MovieItem = () => {
   const [movieItem, setMovieItem] = useState(null);
   const location = useLocation();
   const params = useParams();
+  const backLink = useRef(location.state?.from ?? '/');
 
-  const backLink = location.state?.from ?? '/';
+  // const backLink = location.state?.from ?? '/';
 
   useEffect(() => {
     fetchMovieById(params.movieId)
       .then(data => {
+        // console.log(data);
         setMovieItem(data);
       })
       .catch(error => {
         // console.log(error.message);
-        alert('Page not found');
+        alert(error.message);
       });
   }, [params.movieId]);
 
-  if (!movieItem) {
-    return null;
-  }
+  // if (!movieItem) {
+  //   return null;
+  // }
 
-  // console.log(backLink);
+  // console.log(backLink.current);
 
   return (
     <Box p={3} mt="48px" textAlign="left">
-      <NavLink to={backLink} className={css.goback__btn}>
+      <NavLink to={backLink.current} className={css.goback__btn}>
         Go Back
       </NavLink>
-      <Box display="flex" mt={3}>
-        <div className={css.trend__thumb}>
-          <img
-            className={css.trend__img}
-            width="160"
-            src={`https://image.tmdb.org/t/p/w200/${movieItem.poster_path}`}
-            alt={`${movieItem.original_title}`}
-          />
-        </div>
-        <Box ml={3}>
-          <h3>{movieItem.original_title}</h3>
-          <p className={css.movie__over}>{movieItem.overview}</p>
-          <p className={css.movie__genres}>
-            {movieItem.genres.map(genre => genre.name).join(', ')}
-          </p>
-        </Box>
-      </Box>
-      <Box mt={3} textAlign="center">
-        <Link className={css.movie__add} to="cast">
-          Cast
-        </Link>
-        <Link className={css.movie__add} to="reviews">
-          Reviews
-        </Link>
-      </Box>
-      <Outlet />
+
+      {movieItem && (
+        <>
+          <Box display="flex" mt={3}>
+            <div className={css.trend__thumb}>
+              <img
+                className={css.trend__img}
+                width="160"
+                src={`https://image.tmdb.org/t/p/w200/${movieItem.poster_path}`}
+                alt={`${movieItem.original_title}`}
+              />
+            </div>
+            <Box ml={3}>
+              <h3>{movieItem.original_title}</h3>
+              <p className={css.movie__over}>{movieItem.overview}</p>
+              <p className={css.movie__genres}>
+                {movieItem.genres.map(genre => genre.name).join(', ')}
+              </p>
+            </Box>
+          </Box>
+          <Box mt={3} textAlign="center">
+            <Link className={css.movie__add} to="cast">
+              Cast
+            </Link>
+            <Link className={css.movie__add} to="reviews">
+              Reviews
+            </Link>
+          </Box>
+          <Outlet />
+        </>
+      )}
     </Box>
   );
 };
