488c8b2fc71f643be2bb31edd1f60f67642d293f
vlmakh <vlmakh@gmail.com>
2022-11-24 09:39:01 +0200

    update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -5,11 +5,9 @@ const MAIN_URL = 'https://api.themoviedb.org/3';
 const API_KEY = '7944ae355bdc42ac579681e106149d6b';
 
 // export const perPage = 12;
+// language=uk-UA
 
 const fetchTrends = async () => {
-  // return fetch(`${MAIN_URL}/trending/all/day?api_key=${API_KEY}&page=1`).then(
-  //   response => response.json()
-  // );
   const response = await axios.get(
     `${MAIN_URL}/trending/all/day?api_key=${API_KEY}&page=1`
   );
@@ -17,9 +15,6 @@ const fetchTrends = async () => {
 };
 
 const fetchMovies = async (query, page) => {
-  // return fetch(
-  //   `${MAIN_URL}/search/movie?api_key=${API_KEY}&query=${query}&language=en-US&page=${page}&include_adult=false`
-  // ).then(response => response.json());
   const response = await axios.get(
     `${MAIN_URL}/search/movie?api_key=${API_KEY}&query=${query}&language=en-US&page=${page}&include_adult=false`
   );
@@ -27,19 +22,13 @@ const fetchMovies = async (query, page) => {
 };
 
 const fetchMovieById = async id => {
-  // return fetch(
-  //   `${MAIN_URL}/movie/${id}?api_key=${API_KEY}&language=en-US&include_adult=false`
-  // ).then(response => response.json());
   const response = await axios.get(
-    `${MAIN_URL}/movie/${id}?api_key=${API_KEY}&language=en-US`
+    `${MAIN_URL}/movie/${id}?api_key=${API_KEY}&language=uk-UA`
   );
   return response.data;
 };
 
 const fetchCastById = async id => {
-  // return fetch(
-  //   `${MAIN_URL}/movie/${id}/credits?api_key=${API_KEY}&language=en-US`
-  // ).then(response => response.json());
   const response = await axios.get(
     `${MAIN_URL}/movie/${id}/credits?api_key=${API_KEY}&language=en-US`
   );
@@ -47,9 +36,6 @@ const fetchCastById = async id => {
 };
 
 const fetchReviewsById = async id => {
-  // return fetch(
-  //   `${MAIN_URL}/movie/${id}/reviews?api_key=${API_KEY}&language=en-US&page=1`
-  // ).then(response => response.json());
   const response = await axios.get(
     `${MAIN_URL}/movie/${id}/reviews?api_key=${API_KEY}&language=en-US&page=1`
   );
