80080abeced97a5843298ee61bfb6b6dc7bc95d0
Vlad Makhnenko <vlmakh@gmail.com>
2022-12-26 07:58:38 +0200

    update reviews.js

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -20,28 +20,24 @@ export default function Reviews({ currentLang }) {
     });
   }, [currentLang, params.movieId]);
 
-  if (!reviews.length) {
-    return (
-      <>
+  return (
+    <>
+      {!reviews.length ? (
         <ReviewNot>
           {currentLang === 'uk-UA'
             ? 'Немає інформації'
             : 'No information added'}
         </ReviewNot>
-      </>
-    );
-  }
-
-  return (
-    <>
-      <ReviewList>
-        {reviews.map(review => (
-          <li key={review.id}>
-            <ReviewAuthor>{review.author}</ReviewAuthor>
-            <ReviewText>{review.content}</ReviewText>
-          </li>
-        ))}
-      </ReviewList>
+      ) : (
+        <ReviewList>
+          {reviews.map(review => (
+            <li key={review.id}>
+              <ReviewAuthor>{review.author}</ReviewAuthor>
+              <ReviewText>{review.content}</ReviewText>
+            </li>
+          ))}
+        </ReviewList>
+      )}
     </>
   );
 }
