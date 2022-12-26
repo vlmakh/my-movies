8782fe736395743797e832cc16e4971dd4ba37db
Vlad Makhnenko <vlmakh@gmail.com>
2022-12-26 07:14:40 +0200

    add shadow to trailer

--- src/components/Trailer/Trailer.styled.jsx
+++ src/components/Trailer/Trailer.styled.jsx
@@ -4,6 +4,7 @@ import Iframe from 'react-iframe';
 export const IframeStyled = styled(Iframe)`
   width: 640px;
   height: 360px;
+  box-shadow: 0 0 8px rgba(0, 0, 0, 0.6);
 
   @media (max-width: 900px) {
     max-width: 100%;
