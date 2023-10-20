740e4186e99a53e75ad37a8370b26c0822800add
Vlad Makhnenko <vlmakh@gmail.com>
2023-10-20 08:12:07 +0300

    Update ActorMovies.tsx

--- src/components/ActorMovies/ActorMovies.tsx
+++ src/components/ActorMovies/ActorMovies.tsx
@@ -1,33 +1,57 @@
 import { List, Item } from 'components/BaseComps/BaseComps';
 import { useState, useEffect } from 'react';
-import { useParams, useLocation, NavLink } from 'react-router-dom';
+import { useParams, useLocation, NavLink, useSearchParams } from 'react-router-dom';
 import { fetchMoviesByActor } from 'services/api';
 import { MovieCard } from 'components/MovieCard/MovieCard';
 import { t } from 'i18next';
 import { IMovie } from 'components/types';
+import { PaginationStyled } from 'components/Pagination/Pagination';
 
 export default function ActorMovies() {
   const { actorId } = useParams() as any;
-  const [movies, setMovies] = useState<Array<IMovie>>([]);
+  const [movies, setMovies] = useState<IMovie[]>([]);
   const location = useLocation();
   const lang = t('lang');
+  const [totalPages, setTotalPages] = useState(1);
+  // const [currentPage, setCurrentPage] = useState(1);
+  const [moviesToShow, setMoviesToShow] = useState<IMovie[]>([])
+  const [searchQuery, setSearchQuery] = useSearchParams() as any;
+  const currentPage = Number(searchQuery.get('page'))
+    ? Number(searchQuery.get('page'))
+    : 1;
+  
+  console.log('currentPage: ', currentPage)
+  console.log(moviesToShow)
 
   useEffect(() => {
     fetchMoviesByActor(actorId, lang).then(data => {
-      setMovies(data.cast);
-    });
-  }, [lang, actorId]);
-
-  return (
-    <>
-      <List>
-        {movies
-          .sort((a, b) =>
+      setMovies(data.cast.sort((a: { release_date: any; first_air_date: any; }, b: { release_date: any; first_air_date: any; }) =>
             (b.release_date ?? b.first_air_date) >
             (a.release_date ?? a.first_air_date)
               ? 1
               : -1
-          )
+      ));
+      setMoviesToShow(data.cast.splice(0, 6));
+      // setMoviesToShow([...movies.splice(6 * (e.selected), 6 * e.selected)]);
+      setTotalPages(Math.ceil(data.cast.length / 6));
+    });
+  }, [lang, actorId]);
+
+  // useEffect(() => {
+  //     setMoviesToShow([...movies.splice(0, 6)]);
+  // }, [movies, moviesToShow])
+
+  const handlePageClick = (e: { selected: number; }) => {
+    // setCurrentPage(e.selected + 1);
+    setSearchQuery({ page: e.selected + 1 });
+    
+    setMoviesToShow([...movies.splice(6 * (e.selected), 6 * e.selected)]);
+  };
+
+  return (
+    <>
+      <List>
+        {moviesToShow          
           .map(movie => (
             <Item key={movie.id}>
               <NavLink to={`/movies/${movie.id}`} state={{ from: location }}>
@@ -36,6 +60,20 @@ export default function ActorMovies() {
             </Item>
           ))}
       </List>
+
+      {totalPages > 1 && (
+        <PaginationStyled
+          breakLabel="..."
+          nextLabel=">"
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={5}
+          pageCount={totalPages}
+          previousLabel="<"
+          disabledLinkClassName="disabled"
+          activeClassName="activePage"
+          forcePage={currentPage-1}
+        />
+      )}
     </>
   );
 }
