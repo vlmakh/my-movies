6592bf8f88d93de8b5c1802528d826afe4cf13a8
Vlad Makhnenko <vlmakh@gmail.com>
2023-10-21 16:36:08 +0300

    Update ActorMovies.tsx

--- src/components/ActorMovies/ActorMovies.tsx
+++ src/components/ActorMovies/ActorMovies.tsx
@@ -18,50 +18,44 @@ export default function ActorMovies() {
   const location = useLocation();
   const lang = t('lang');
   const [totalPages, setTotalPages] = useState(1);
-  const [moviesToShow, setMoviesToShow] = useState<IMovie[]>([]);
   const [searchQuery, setSearchQuery] = useSearchParams() as any;
   const currentPage = Number(searchQuery.get('page'))
     ? Number(searchQuery.get('page'))
     : 1;
-
   const itemsPerPage = 20;
 
   useEffect(() => {
+    const start = itemsPerPage * (currentPage - 1);
+    const end = itemsPerPage * currentPage;
+
     fetchMoviesByActor(actorId, lang).then(data => {
       setMovies(
-        data.cast.sort(
-          (
-            a: { release_date: any; first_air_date: any },
-            b: { release_date: any; first_air_date: any }
-          ) =>
-            (b.release_date ?? b.first_air_date) >
-            (a.release_date ?? a.first_air_date)
-              ? 1
-              : -1
-        )
+        data.cast
+          .sort(
+            (
+              a: { release_date: any; first_air_date: any },
+              b: { release_date: any; first_air_date: any }
+            ) =>
+              (b.release_date ?? b.first_air_date) >
+              (a.release_date ?? a.first_air_date)
+                ? 1
+                : -1
+          )
+          .filter((_: any, ind: number) => ind >= start && ind < end)
       );
-
-      setMoviesToShow([
-        ...data.cast.filter((_: any, ind: number) => ind >= 0 && ind < itemsPerPage),
-      ]);
 
       setTotalPages(Math.ceil(data.cast.length / itemsPerPage));
     });
-  }, [lang, actorId]);
+  }, [lang, actorId, currentPage]);
 
   const handlePageClick = (e: { selected: number }) => {
     setSearchQuery({ page: e.selected + 1 });
-
-    const start = itemsPerPage * e.selected;
-    const end = itemsPerPage * (e.selected + 1);
-
-    setMoviesToShow([...movies.filter((el, ind) => ind >= start && ind < end)]);
   };
 
   return (
     <>
       <List>
-        {moviesToShow.map(movie => (
+        {movies.map(movie => (
           <Item key={movie.id}>
             <NavLink to={`/movies/${movie.id}`} state={{ from: location }}>
               <MovieCard movie={movie} />
